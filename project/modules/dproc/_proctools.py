51dd7c7d5ebc5dff59f06ce30f7f9b4237ae5336
Jason <jgodmere808@gmail.com>
2019-12-18 21:21:25 -1000

    Worked out the basic dproc.compute() structure without testing. Still working out first proc function dproc.prep_train_data()

---
+++ project/modules/dproc/_proctools.py
@@ -0,0 +1,106 @@
+
+
+#standard libraries
+import math
+import time
+import datetime
+
+import numpy as np
+
+
+def unix_to_date(self, unix):#input unix time as string or int
+	#when using to display on screen, add to UTC unix param to offset for your timezone
+	return datetime.datetime.utcfromtimestamp(unix).strftime('%Y-%m-%dT%H:%M:%S.%f0Z')
+	#RETURNS UTC, confirmed
+
+
+def date_to_unix(self, date):
+	unix = datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%f0Z')
+	unix = unix.timestamp() - 36000#sets it to UTC
+	return unix
+
+
+def scale(self, target_array, new_range=[0, 1], custom_scale=[0, 0], return_params=False):
+	'''
+	Parameters:
+		target_array   : one dimension array "target" for scaling
+		new_range      : value range of data after scaling
+		custom_scale   : optional - value range reference
+			This is used when user wants to scale based on custom values not in target_array
+		return_params  : "return scaled_data, params" rather than "return scaled_data"
+	'''
+	target_array = list(target_array)
+	if custom_scale != [0, 0]:
+		min_val = min(custom_scale)
+		max_val = max(custom_scale)
+	else:
+		min_val = min(target_array)
+		max_val = max(target_array)
+	new_width = abs(new_range[0] - new_range[1])
+
+	#sets target_array values between 0 and 1
+	scaled_data = np.divide(np.subtract(target_array, min_val), (max_val - min_val))
+
+	if new_range != [0, 1]:
+		#adjusts to non-standard new_range if requested
+		scaled_data = np.add(np.multiply(scaled_data, new_width), new_range[0])
+
+	if return_params == True:
+		orig_range = [min_val, max_val]
+		scaled_zero = self.FeatureScale([0], orig_range=orig_range, new_range=new_range)
+		scaled_zero = scaled_zero[0]
+		params = {'orig_range': orig_range, 
+				  'new_range': feature_range,
+				  'scaled_zero': scaled_zero}
+		return scaled_data, params
+
+	return scaled_data
+
+
+def print_progress_bar(iteration, total, prefix = '', suffix = ''):
+	#these varibales used to be parameters but there is no need to have them change
+	length = 49
+	fill = '/'
+	printEnd = "\r"
+	decimals = 1
+
+	'''
+	Call in a loop to create terminal progress bar
+	Parameters:
+		iteration   - Required  : current iteration (Int)
+		total       - Required  : total iterations (Int)
+		prefix      - Optional  : prefix string (Str)
+		suffix      - Optional  : suffix string (Str)
+		decimals    - Optional  : positive number of decimals in percent complete (Int)
+		length      - Optional  : character length of bar (Int)
+		fill        - Optional  : bar fill character (Str)
+		printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+	'''
+	percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+	filledLength = int(length * iteration // total)
+	bar = fill * filledLength + '-' * (length - filledLength)
+	print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = printEnd)
+	# Print New Line on Complete
+	if iteration == total: 
+		print()
+
+
+def thread_monitor(progress, compute_total, thread_total):
+	'''
+	Parameters:
+		progress       : number of computed items from each thread (shared dict)
+		compute_total  : total number of items to be computed (int)
+	'''
+	prog_count = 0
+	threads = 1
+	while prog_count < compute_total:
+		prog_part = progress['part']
+		print_progress_bar(prog_count, compute_total, 
+						   suffix=f' | {prog_part}, threads: {threads}/{thread_total}')
+		
+		for key, val in progress.items():
+			if key == 'threads':
+				threads = len(val) + 1 #plus 1 for monitoring thread
+			elif key == 'count':
+				prog_count = sum(val.values())
+
--- project/modules/dproc/dproc.py
+++ project/modules/dproc/dproc.py
@@ -8,58 +8,13 @@ import time
 import datetime
 
 import numpy as np
+import pandas as pd
+
+import multiprocessing
+from multiprocessing import Manager
 
 
-def unix_to_date(self, unix):#input unix time as string or int
-	#when using to display on screen, add to UTC unix param to offset for your timezone
-	return datetime.datetime.utcfromtimestamp(unix).strftime('%Y-%m-%dT%H:%M:%S.%f0Z')
-	#RETURNS UTC, confirmed
-
-
-def date_to_unix(self, date):
-	unix = datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%f0Z')
-	unix = unix.timestamp() - 36000#sets it to UTC
-	return unix
-
-
-def scale(self, target_array, new_range=[0, 1], custom_scale=[0, 0], return_params=False):
-	'''
-	Parameters:
-		target_array   : one dimension array "target" for scaling
-		new_range      : value range of data after scaling
-		custom_scale   : optional - value range reference
-			This is used when user wants to scale based on custom values not in target_array
-		return_params  : "return scaled_data, params" rather than "return scaled_data"
-	'''
-	target_array = list(target_array)
-	if custom_scale != [0, 0]:
-		min_val = min(custom_scale)
-		max_val = max(custom_scale)
-	else:
-		min_val = min(target_array)
-		max_val = max(target_array)
-	new_width = abs(new_range[0] - new_range[1])
-
-	#sets target_array values between 0 and 1
-	scaled_data = np.divide(np.subtract(target_array, min_val), (max_val - min_val))
-
-	if new_range != [0, 1]:
-		#adjusts to non-standard new_range if requested
-		scaled_data = np.add(np.multiply(scaled_data, new_width), new_range[0])
-
-	if return_params == True:
-		orig_range = [min_val, max_val]
-		scaled_zero = self.FeatureScale([0], orig_range=orig_range, new_range=new_range)
-		scaled_zero = scaled_zero[0]
-		params = {'orig_range': orig_range, 
-				  'new_range': feature_range,
-				  'scaled_zero': scaled_zero}
-		return scaled_data, params
-
-	return scaled_data
-
-
-def compute(raw_data, func, threads=multiprocessing.cpu_count(), name='...'):
+def compute(raw_data, func, threads=multiprocessing.cpu_count(), name=''):
 	'''
 	parameters:
 		raw_data  : {key: pd.DataFrame(), ...} (dict) key is printed to console
@@ -92,113 +47,79 @@ def compute(raw_data, func, threads=multiprocessing.cpu_count(), name='...'):
 	#initializes monitoring thread
 	proc_monitor = Process(target=thread_monitor, args=(progress, compute_total, threads))
 	proc_monitor.start()
+
 	for key, df in raw_data.items():
-		pass
+		
+		#current df total rows
+		df_len = len(df.index)
+		#rough number of rows each thread is responsible for computing
+		proc_len = int(df_len / proc_threads)
 
+		if proc_threads > df_len:
+			print('WARNING: More threads than compute items, dproc.compute()')
 
-	#=======================================old code past this point
+		#first index of the df (may not be 0)
+		init_index = df.index[0]
+		#end index of the df (may not be df_len)
+		last_index = df.index[-1]
 
+		#initializes threads with a specified proc interval and proc_num
+		start_index = 0
+		procs = []
+		for proc_num in range(proc_threads):
+			#determines the last index for compute interval
+			end_index = start_index + proc_len
+			if end_index > last_index - init_index:
+				end_index = last_index - init_index
 
-	'''
-	self.new_data = raw_data
-	self.data_index = data_index
+			proc_interval = df.index[start_index:end_index]
 
-	#used to calculate time delay between iterations of following loop
-	init_time = time.time()
-
-
-	if self.proc_type == 'training_data':
-		self.InitTrainingData()
-
-
-	#This sets up a batch of data for each processing thread so that all data is 
-	#   processed once the last thread is reserved for monitoring progress and 
-	#   does not receive a batch of data
-	proc_threads = Preprocessor.thread_count - 1
-	proc_length = math.ceil(self.data_index['datapoints'] / proc_threads)
-	proc_intervals = []
-	last_index = raw_data.index[-1]
-	start_index = 0
-	for thread in range(proc_threads):
-		end_index = start_index + proc_length
-		custom_start = start_index - self.data_index['prediction_steps']
-
-		if thread == 0:
-			proc_intervals.append(raw_data.index[start_index:end_index])
-		elif thread == proc_threads-1:
-			end_index = raw_data.index[-1]
-			#by default python array slicing ignores last point so add one index value worth
-			proc_intervals.append(raw_data.index[custom_start:end_index])
-		elif custom_start < 0:
-			custom_start = 0
-			proc_intervals.append(raw_data.index[custom_start:end_index])
-		else:
-			proc_intervals.append(raw_data.index[custom_start:end_index])
-
-		start_index = end_index
-
-	manager = Manager()
-	new_proc_data = manager.dict()
-	proc_status = manager.dict()
-
-	#uses proc_data_all in loop to set up each process for multithreading, order does not matter
-	#MULTIPROC SETUP
-	print("Preping Data...")
-	procs = []
-
-	#initializes the monitoring thread
-	proc_monitor = Process(target=self.ProcMonitor, args=(proc_status,))
-	procs.append(proc_monitor)
-	procs[0].start()
-	for proc_num, proc_interval in enumerate(proc_intervals):
-		if self.proc_type == 'training_data':
-			proc = Process(target=self.MultiprocTrainingSetup, args=(proc_interval, 
-																	new_proc_data,
-																	proc_status,
-																	proc_num,))
+			proc = Process(target=func, args=(proc_interval, df, progress, proc_num,))
 			procs.append(proc)
 			proc.start()
 
-	#ends multithreaded processes
-	for proc in procs:
-		proc.join()
-	for proc_num, dataframe in new_proc_data.items():
-		self.new_data.loc[proc_intervals[proc_num], :] = dataframe.loc[proc_intervals[proc_num], :]
+		#ends multithreaded processes
+		for proc in procs:
+			proc.join()
 
 
-	raw_data = self.new_data.copy()
-
-	#wipes new_proc_data and proc_status
-	new_proc_data = manager.dict()
-	proc_status = manager.dict()
-
-	print("\nProcessing Data...")
-	#MULTIPROC FINAL
-	procs = []
-	#initializes the monitoring thread
-	proc_monitor = Process(target=self.ProcMonitor, args=(proc_status,))
-	procs.append(proc_monitor)
-	procs[0].start()
-	for proc_num, proc_interval in enumerate(proc_intervals):
-		if self.proc_type == 'training_data':
-			proc = Process(target=self.MultiprocTrainingFinal, args=(proc_interval, 
-																	self.data_index['prediction_steps'],
-																	new_proc_data,
-																	proc_status,
-																	proc_num,))
-		procs.append(proc)
-		proc.start()
-	#ends multithreaded processes
-	for proc in procs:
-		proc.join()
-	for proc_num, dataframe in new_proc_data.items():
-		self.new_data.loc[proc_intervals[proc_num], :] = dataframe.loc[proc_intervals[proc_num], :]
+			start_index = end_index
 
 
-	total_time = time.time() - init_time
-	print(f"\nTotal Duration: {total_time}")
+def proc_id(part, proc_num):
+	return f'{part}|{proc_num}'
 
-	print(self.new_data.head(30))
 
-	return self.new_data
+def prep_train_data(interval, df, progress, proc_num=0):
 	'''
+	Parameters:
+		interval      : list of indexes that this function will compute (list(int))
+		df            : complete dataset, is only read for reference (pd.DataFrame())
+		progress      : shared dict for tracking progress across threads (Manager.dict())
+		proc_num      : unique thread id assigned by dproc.compute() when multithreading
+
+	returns computed slice of ref_df outlined by proc_interval
+	'''
+
+	proc_id = proc_id(progress['part'], proc_num)
+
+	#data that will be computed
+	df_slice = df.loc[proc_interval, :]
+
+	#iterates through target df_slice rows
+	init_index = df_slice.index[0]
+	for index, row in df_slice.iterrows():
+		for col in initial_data.columns:
+
+			if 'is_nan' in col:
+				if np.isnan(row[col]):
+					new_data.at[index, col] = 1 #is_nan = true
+
+			if 'average_price' in col:
+				low = initial_data.at[index, col.replace('average_price', 'price_low')]
+				high = initial_data.at[index, col.replace('average_price', 'price_high')]
+				average = (low + high) / 2
+				new_data.at[index, col] = average
+
+		if (index - init_index) % 1000 == 0:
+			progress[]
