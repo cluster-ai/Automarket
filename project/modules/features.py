(fixed) Database.backfill() would load empty datapoints to historical data even if there was no internet connection. Added features.smooth(), right now it only averages based on adjacent values. no added unittesting

project/modules/features.py
@@ -4,6 +4,7 @@ import datetime
 
 #third-party packages
 import pandas as pd
+import numpy as np
 
 #79 character absolute limit
 ###############################################################################
@@ -106,10 +107,10 @@ def delta(historical):
 			continue
 
 		#/price/ features
-		features.at[index, 'price_average'] = delta(historical,
+		'''features.at[index, 'price_average'] = delta(historical,
 													'price_average',
 													prev_index,
-													index)
+													index)'''
 		features.at[index, 'price_low'] = delta(historical,
 												'price_low',
 												prev_index,
@@ -128,7 +129,79 @@ def delta(historical):
 												   'trades_count',
 												   prev_index,
 												   index)
+
+		#used for continuity
+		features.at[index, 'time_period_start'] = historical.at[index, 'time_period_start']
+
 		#updates prev_index
 		prev_index = index
 
 	return features
+
+
+def smooth(historical, time_increment):
+	'''
+	
+	THIS CAN BE USED TO SIMPLIFY DATA FOR LARGER SEQUENCES
+	IT ALSO HELPS REMOVE OUTLIERS FROM THE DATA
+
+	This data iterates through DataFrame and averages each
+	value with values directly adjacent to it
+
+	Creates the following features
+		/price/
+		- price_average
+		- price_low
+		- price_high
+		/other/
+		- volume_traded
+		- trades_count
+
+	Uses follwing historical data
+		- price_average
+		- price_low
+		- price_high
+		- volume_traded
+		- trades_count
+
+	Assumptions:
+		- historical.index values are incrementing evenly and continuously
+		- historical.index values are increment by "time_increment" seconds
+		- historical.isnan values are 0 if False and 1 if True
+	'''
+
+	#sets 'time_period_start' as the index of historical
+	historical.set_index('time_period_start', drop=False, inplace=True)
+
+	#creates a complete copy for the new data to be saved
+	#this prevents changed values from influencing the algorithm
+	data = historical.copy()
+
+	#iterates through historical and converters data values
+	for index, row in historical.iterrows():
+
+		prev_index = index - time_increment
+		next_index = index + time_increment
+
+		#verifies adjacent indexes exist
+		if (prev_index not in historical.index or 
+				next_index not in historical.index):
+			data.loc[index, :] = np.nan
+			continue
+
+		#iterates through columns
+		for col in historical.columns:
+			#verifies col is one that will be converted
+			if col in ['price_high', 'price_low',
+					   'volume_traded', 'trades_count']:
+
+				#loads values
+				prev_val = historical.at[prev_index, col]
+				next_val = historical.at[next_index, col]
+				val = row[col]
+
+				#creates new val and saves it to data
+				new_val = (prev_val + next_val + val) / 3
+				data.at[index, col] = new_val
+
+	return data