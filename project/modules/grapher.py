abc3a1eb633417b60efdf92459cec782d465e61f
Jason <jgodmere808@gmail.com>
2020-02-08 21:09:10 -1000

    Confirmed kivy graphing implementation for Version 1 of application. Basic Kivy app added to grapher.py. no added unit testing

--- project/modules/grapher.py
+++ project/modules/grapher.py
@@ -1,12 +1,23 @@
 
+#kivy
+import kivy
+kivy.require('1.11.1') # replace with your current kivy version !
+from kivy.app import App
+from kivy.uix.label import Label
+
 #standard libraries
 from itertools import count
 
 #third-party packages
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 #local modules
+from define import *
+import modules.preproc as preproc
+import modules.features as features
 
 #79 character absolute limit
 ###############################################################################
@@ -15,6 +26,9 @@ from matplotlib.animation import FuncAnimation
 ########################################################################
 
 '''
+NOTE: TO CREATE A GRAPH, USE ONLY THE "graph" FUCNTION
+	  DO NOT CREATE INSTANCE OF GRAPHER (its unnecessary)
+
 The "Graph" class will be used as a continuously updating 
 representation of data. The built in features of this class
 will be for data stored in the database, any other data must 
@@ -23,25 +37,29 @@ be loaded into it via argument.
 For version 1 (if time permits), this module would benefit from
 being nested in a UI allowing the user to change graph data/options
 without having to reopen it manually. (use kivy UI framework)
+
+NOTE: Had to download the following for it to work
+pip install --upgraade pip wheel setuptools
+pip install docutils pygments pypiwin32 kivy.deps.sdl2 kivy.deps.glew
+pip install kivy.deps.gstreamer
+pip install kivy.deps.angle
+pip install --upgrade kivy
 '''
 
-from define import *
-import numpy as np
-import modules.preproc as preproc
-import modules.features as features
+class Grapher(App):
+
+    def build(self):
+        return Label(text='Hello world')
 
 '''
-
-'''
-
 class Grapher():
 
 	def __init__(self):
-		'''
+		''
 		IMPORTANT:
 		Develope this class to utilize the database directly.
-		'''
-		self.datapoints = 300 #datapoints
+		''
+		self.datapoints = 100 #datapoints
 		self.raw = Database.historical('KRAKEN_BTC_5MIN')
 		self.raw = self.raw.loc[1537761000:1550275800, :]
 		self.delta = features.delta(self.raw)
@@ -55,7 +73,7 @@ class Grapher():
 		start_time = end_time - (300 * self.datapoints)
 		interval = abs(end_time - start_time)
 
-		self.index += 1
+		#self.index += 1
 
 		display_raw = self.raw.loc[start_time:end_time, 'price_high']
 		display_smooth = self.smooth.loc[start_time:end_time, 'price_high']
@@ -79,6 +97,9 @@ class Grapher():
 		plt.plot(display_smooth)
 
 
-	def display(self):
-		ani = FuncAnimation(plt.gcf(), self.animate, interval=100)
-		plt.show()
+def graph():
+	graph = Grapher()
+
+	ani = FuncAnimation(plt.gcf(), graph.animate, interval=100)
+	plt.show()
+'''
